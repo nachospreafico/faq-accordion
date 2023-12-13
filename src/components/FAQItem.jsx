1c4bb53f237453b888ca8c091b3d239b8d0d101b
Ignacio Spreafico <nachospreafico06@gmail.com>
2023-12-13 13:09:41 +0100

    more changes

--- src/components/FAQItem.jsx
+++ src/components/FAQItem.jsx
@@ -1,21 +1,37 @@
 import Plus from "./../assets/images/icon-plus.svg";
 import Minus from "./../assets/images/icon-minus.svg";
+import { useState } from "react";
 
 const FAQItem = ({ q, a, border, clicked, onItemClick }) => {
+  const [iconClicked, setIconClicked] = useState(false);
+
+  const handleItemClick = () => {
+    if (!iconClicked) {
+      onItemClick();
+    }
+    setIconClicked(false);
+  };
+
+  const handleIconClick = (e) => {
+    e.stopPropagation();
+    setIconClicked(true);
+    onItemClick();
+  };
+
   return (
     <li className={`flex flex-col gap-4 ${border ? "border-b-2 pb-4" : ""}`}>
       <div className="flex flex-row gap-2 justify-between items-center">
         <p
-          onClick={onItemClick}
+          onClick={handleItemClick}
           className="font-bold text-lg lg:hover:text-[#B12EEC] duration-300 cursor-pointer"
         >
           {q}
         </p>
         <img
           src={clicked ? Minus : Plus}
-          alt="Plus icon"
+          alt={clicked ? "Minus icon" : "Plus icon"}
           className="cursor-pointer"
-          onClick={onItemClick}
+          onClick={handleIconClick}
         ></img>
       </div>
       {clicked && <p className="text-sm text-gray-500">{a}</p>}
